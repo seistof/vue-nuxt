a4484d6c6387dbab5e9eb2fa6d9ade9c2906d99c
seistof <seistof@gmail.com>
2020-12-15 00:45:51 +0300

    +about, +user, ~routes

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores inventore nobis voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quaerat?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </section>
+</template>
---
+++ pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>User {{ $route.params.id }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  validate(context) {
+    return true;
+  },
+};
+</script>
