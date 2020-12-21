80faf502a522ff08f19aeb87d202962e79036768
seistof <seistof@gmail.com>
2020-12-21 20:04:59 +0300

    reset

--- pages/about.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section>
-    <h1>About page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores inventore nobis voluptatem!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quaerat?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-  </section>
-</template>
